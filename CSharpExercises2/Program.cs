d4c668bf2260935f190764346291103eda8106a6
Brandon <brandoncyu@gmail.com>
2019-08-27 10:46:37 -0700

    new repo for csharp

---
+++ CSharpExercises2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpExercises2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
